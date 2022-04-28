b832b31325c00bc048c3d0e7ce07b603bc71d9d2
devyosse <narutoshippuden884@gmail.com>
2022-04-28 14:35:36 +0200

    update

--- index.php
+++ index.php
@@ -13,9 +13,20 @@
         <a href="GalaxyPhone/GalaxyM.php">Galaxy M</a>
         <a href="GalaxyPhone/GalaxyA.php">Galaxy A</a>
         <a href="GalaxyPhone/GalaxyS.php">Galaxy S</a>
-        <a href="GalaxyPhone/GlaxyFold.php">Galaxy Fold</a>
+        <a href="GalaxyPhone/GalaxyFold.php">Galaxy Fold</a>
+        <a href="User/Inscription.php">Inscription</a>
+        <a href="User/Connexion.php">Connexion</a>
     </nav>
 
+
+
+    <h1>Bienvenue connecte-toi pour avoir accès au site dans son intégralité</h1>
+
+    <p>
+        Tu es ici car tu te veux te renseigner sur les dernières actualités Samsung ? <br>
+        Tu es bien tombés, mon site te documentera sur toutes les gammes Samsung.<br>
+        Ainsi tu pourra choisir le smartphone qui te correspond le mieux.
+    </p>
 </body>
 </html>
 
