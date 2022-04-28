a1ff47b04e7a4ae401f01a00782a41ee05471736
devyosse <narutoshippuden884@gmail.com>
2022-04-28 10:58:16 +0200

    first commit

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="galaxy.css">
+    <title>SamsungV2</title>
+</head>
+<body>
+
+    <nav>
+        <a href="GalaxyPhone/GalaxyM.php">Galaxy M</a>
+        <a href="GalaxyPhone/GalaxyA.php">Galaxy A</a>
+        <a href="GalaxyPhone/GalaxyS.php">Galaxy S</a>
+        <a href="GalaxyPhone/GlaxyFold.php">Galaxy Fold</a>
+    </nav>
+
+</body>
+</html>
+
+
+
 <?php
