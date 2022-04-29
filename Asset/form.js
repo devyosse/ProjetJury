c054ccd03e644618dc15be30f9cd505937eeedce
devyosse <narutoshippuden884@gmail.com>
2022-04-29 11:53:20 +0200

    update

---
+++ Asset/form.js
@@ -0,0 +1,15 @@
+let password = document.getElementById("id-password");
+let passwordConfirm = document.getElementById("id-password-repeat");
+
+
+function checkPassword(){
+    if (password.value !== passwordConfirm.value){
+        passwordConfirm.setCustomValidity("Les mots de passe ne correspondent pas.");
+    }
+    else{
+        passwordConfirm.setCustomValidity('');
+    }
+}
+
+password.addEventListener('change', checkPassword);
+passwordConfirm.addEventListener('keyup', checkPassword);
