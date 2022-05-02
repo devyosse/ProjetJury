fcedcfc126f4e1593f2eda72720a360cda9fb390
devyosse <narutoshippuden884@gmail.com>
2022-05-02 10:37:37 +0200

    update

--- includes.php
+++ includes.php
@@ -4,3 +4,14 @@
 require __DIR__ . "User/Inscription.php";
 require __DIR__ . "User/Connexion.php";
 
+require __DIR__ . '/Model/Database.php';
+require __DIR__ . '/Model/Entity/AbstractEntity.php';
+require __DIR__ . '/Model/Entity/Product.php';
+require __DIR__ . '/Model/Entity/Comment.php';
+require __DIR__ . '/Model/Entity/Role.php';
+require __DIR__ . '/Model/Entity/User.php';
+
+require __DIR__ . '/Model/Manager/RoleManager.php';
+require __DIR__ . '/Model/Manager/UserManager.php';
+require __DIR__ . '/Model/Manager/CommentManager.php';
+require __DIR__ . '/Model/Manager/ProductManager.php';
