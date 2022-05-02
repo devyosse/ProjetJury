8214690318c3a4ec32b418791b7e1a23ae6d8e06
devyosse <narutoshippuden884@gmail.com>
2022-05-02 15:02:37 +0200

    update

--- Partials/footer.php
+++ Partials/footer.php
@@ -1,1 +1,11 @@
-<?php
+
+<h1>Bienvenue connecte-toi pour avoir accès au site dans son intégralité</h1>
+
+    <p>
+Tu es ici car tu te veux te renseigner sur les dernières actualités Samsung ? <br>
+Tu es bien tombés, mon site te documentera sur toutes les gammes Samsung.<br>
+Ainsi tu pourra choisir le smartphone qui te correspond le mieux.
+    </p>
+</body>
+</html>
+
--- Partials/header.php
+++ Partials/header.php
@@ -1,1 +1,19 @@
-<?php
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="galaxy.css">
+    <title>SamsungV2</title>
+</head>
+<body>
+
+    <nav>
+        <a href="../GalaxyPhone/GalaxyM.php">Galaxy M</a>
+        <a href="../GalaxyPhone/GalaxyA.php">Galaxy A</a>
+        <a href="../GalaxyPhone/GalaxyS.php">Galaxy S</a>
+        <a href="../GalaxyPhone/GalaxyFold.php">Galaxy Fold</a>
+        <a href="../User/Inscription.php">Inscription</a>
+        <a href="../User/Connexion.php">Connexion</a>
+    </nav>
