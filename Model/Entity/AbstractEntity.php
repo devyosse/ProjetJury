846737e59fa817db76bbe4c2b9872b2f46421f7b
devyosse <narutoshippuden884@gmail.com>
2022-05-02 10:29:48 +0200

    update

---
+++ Model/Entity/AbstractEntity.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class AbstractEntity
+{
+    private ?int $id = null;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     */
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+}
