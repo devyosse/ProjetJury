a10072fbb154b029de06f61128e0198613e0eed4
devyosse <narutoshippuden884@gmail.com>
2022-05-06 11:28:51 +0200

    update

--- Model/Entity/Comment.php
+++ Model/Entity/Comment.php
@@ -1,6 +1,64 @@
 <?php
 
-class Comment
+namespace App\Model\Entity;
+
+use AbstractEntity;
+use Product;
+use User;
+
+class Comment extends AbstractEntity
 {
+    private string $content;
+    private User $author;
+    private Product $product;
+
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return User
+     */
+    public function getAuthor(): User
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param User $author
+     */
+    public function setAuthor(User $author): void
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return product
+     */
+    public function getProduct(): Product
+    {
+        return $this->product;
+    }
+
+    /**
+     * @param Product $product
+     */
+    public function setProduct(\Product $product): void
+    {
+        $this->product = $product;
+    }
 
 }
--- Model/Entity/Role.php
+++ Model/Entity/Role.php
@@ -1,6 +1,26 @@
 <?php
 
-class Role
-{
+use App\Model\Entity\AbstractEntity;
 
+class Role extends AbstractEntity
+{
+    private ?string $roleName;
+
+    /**
+     * @return string|null
+     */
+    public function getRoleName(): ?string
+    {
+        return $this->roleName;
+    }
+
+    /**
+     * @param string|null $roleName
+     * @return \App\Model\Entity\Role
+     */
+    public function setRoleName(?string $roleName): self
+    {
+        $this->roleName = $roleName;
+        return $this;
+    }
 }
