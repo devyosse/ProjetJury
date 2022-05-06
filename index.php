1f07da9cc44ea97b4b6f8978ee93d855b0ea8620
devyosse <narutoshippuden884@gmail.com>
2022-05-06 11:39:50 +0200

    update

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
 <?php
-require __DIR__ . '../includes.php';
+require __DIR__ . './includes.php';
+
+
+session_start();
+
+$page = AbstractRouter::secure($_GET['c']) ?? 'home';
+$method = AbstractRouter::secure($_GET['a']) ?? 'index';
+
+// Defining the right controller.
+switch ($page) {
+    case 'home':
+        HomeRouter::route();
+        break;
+    case 'user':
+        UserRouter::route($method);
+        break;
+    case 'product':
+        ProductRouter::route($method);
+        break;
+    default:
+        (new ErrorController())->error404($page);
+}
+
