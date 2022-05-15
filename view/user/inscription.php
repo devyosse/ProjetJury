dc10e813f7d965510fe78fad0684116e454ab4af
devyosse <narutoshippuden884@gmail.com>
2022-05-15 12:11:01 +0200

    update

--- view/user/inscription.php
+++ view/user/inscription.php
@@ -1,29 +1,29 @@
+ <link rel="stylesheet" href="/Asset/style.css">
 
+<body>
+
+</body>
+</html>
 <form method="post" action="/index.php?c=user&a=register" id="id-form" class="form">
 
     <div class="class-username">
-        <label for="id-username">Username</label>
-        <input type="text" name="username" id="id-username" required>
+        <input type="text" name="username" id="id-username" placeholder="Username" required>
     </div>
 
     <div class="class-age">
-        <label for="id-age">Your age</label>
-        <input type="number" name="age" id="id-age" max="120" required>
+        <input type="number" name="age" id="id-age" max="120" placeholder="Your Age" required>
     </div >
 
     <div class="class-mail">
-        <label for="id-email">Your Email</label>
-        <input type="text" name="email" id="id-email" minlength="4" required>
+        <input type="text" name="email" id="id-email" minlength="4" placeholder="Your Email" required>
     </div>
 
     <div class="class-password">
-        <label for="id-password">Password</label>
-        <input type="password" name="password" id="id-password" minlength="6" maxlength="24" required>
+        <input type="password" name="password" id="id-password" minlength="6" maxlength="24" placeholder="Password" required>
     </div>
 
     <div class="class-password-repeat">
-        <label for="id-password-repeat">Password-repeat</label>
-        <input type="password" name="password-repeat" id="id-password-repeat" required>
+        <input type="password" name="password-repeat" id="id-password-repeat" placeholder="Password-repeat" required>
     </div>
 
     <div class="class-submit">
